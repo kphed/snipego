255b97908141459b9947e3ce744f98917d064763
Khoa Phan <khoaphan89@gmail.com>
2015-09-29 15:11:04 -0700

    Add and utilize angular-svg-round-progressbar for homepage

--- public/js/angular/controller/MainCtrl.js
+++ public/js/angular/controller/MainCtrl.js
@@ -2,6 +2,18 @@
 
 angular.module('SnipeGo.MainCtrl', ['SnipeGo'])
   .controller('MainCtrl', ['$scope', function($scope) {
-
+    $scope.getStyle = function(){
+      var transform = 'translateY(-50%) ' + 'translateX(-50%)';
+      console.log('calling getStyle');
+      return {
+          'top': '50%',
+          'bottom': 'auto',
+          'left': '50%',
+          'transform': transform,
+          '-moz-transform': transform,
+          '-webkit-transform': transform,
+          'font-size': 75 + 'px'
+      };
+    };
   }]
 );
