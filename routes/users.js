215635de9b9ffd4b4792956a7a6be81ff93629c9
Khoa Phan <khoaphan89@gmail.com>
2015-09-29 18:31:16 -0700

    Add route to return a json object revealing whether a user is authenticated or not

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ var passport = require('passport');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log('whaaat');
   res.send('respond with a resource');
 });
 
@@ -12,8 +13,12 @@ router.get('/auth/steam', passport.authenticate('steam'), function(req, res, nex
 });
 
 router.get('/auth/steam-callback', passport.authenticate('steam'), function(req, res) {
-  console.log('steam auth callback...');
+  // console.log('steam auth callback... what is the req session?', req.session);
   res.send('success');
+});
+
+router.get('/auth/is-authenticated', function(req, res) {
+  console.log('passing back is authenticated data');
 });
 
 module.exports = router;
