2dc00dfa41a55f3d1e0f7c91b48b950563fb2564
Khoa Phan <khoaphan89@gmail.com>
2015-09-29 21:31:40 -0700

    Working steam authentication

--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,18 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  console.log('whaaat');
-  res.send('respond with a resource');
-});
-
 router.get('/auth/steam', passport.authenticate('steam'), function(req, res, next) {
-  console.log('steam authentication...');
+  //nothing called here, all authentication handled in passport
 });
 
 router.get('/auth/steam-callback', passport.authenticate('steam'), function(req, res) {
   console.log('steam auth callback... what is the req session?', req.session);
-  res.send('success');
+  res.redirect('/#/');
 });
 
 router.get('/auth/is-authenticated', function(req, res) {
   console.log('passing back is authenticated data');
+  res.json(req.session.passport.user);
 });
 
 module.exports = router;
