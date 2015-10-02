d9a2a0b70ba9429e2f4ccf0c899d5d1fae579a1e
Khoa Phan <khoaphan89@gmail.com>
2015-10-02 16:04:52 -0700

    Fetch user's inventory and display via table, with market_hash_name popovers

--- client/public/js/angular/controller/DepositCtrl.js
+++ client/public/js/angular/controller/DepositCtrl.js
@@ -2,11 +2,38 @@
 
 angular.module('SnipeGo.DepositCtrl', ['SnipeGo', 'SnipeGo.Services'])
   .controller('DepositCtrl', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
+     $scope.inventoryLoading = false;
+
+     $scope.items = [];
+
+     $scope.addItems = function(items, descriptions) {
+      var row = [];
+      var tempObj = {};
+      console.log('calling addItems');
+      return Object.keys(items).map(function(id) {
+        var item = items[id];
+        var description = descriptions[item.classid + '_' + (item.instanceid || '0')];
+        for (var key in description) {
+          item[key] = description[key];
+        }
+        item.contextid = 2;
+        tempObj.market_hash_name = item.market_hash_name;
+        tempObj.icon_url = item.icon_url;
+        row.push(tempObj);
+        tempObj = {};
+        if (row.length === 5) {
+          $scope.items.push(row);
+          row = [];
+        }
+        return item;
+      });
+    };
+
      $scope.grabInventory = function() {
-      console.log('calling grab inventory', $rootScope.user);
-      $http.get('/users/inventory')
+      $http.post('/users/inventory', {steamid: $rootScope.user.id})
         .success(function(resp) {
-          console.log('here is your response', resp);
+          console.log('inventory: ', resp.rgDescriptions);
+          $scope.addItems(resp.rgInventory, resp.rgDescriptions);
         });
     };
   }]
--- server/models/database.js
+++ server/models/database.js
@@ -5,6 +5,12 @@ var connectionString = require(path.join(__dirname, '../db', 'config'));
 var client = new pg.Client(connectionString);
 client.connect();
 
-var createTableUsers = client.query("CREATE TABLE IF NOT EXISTS users(id SERIAL, steamid bigint, avatar text, item_ids bigint[])");
+client.query("DROP TABLE IF EXISTS users");
 
-createTableUsers.on('end', function() { client.end(); });
+client.query("DROP TABLE IF EXISTS items");
+
+client.query("CREATE TABLE IF NOT EXISTS users(id SERIAL, steamid bigint, avatar text, items text[])");
+
+client.query("CREATE TABLE IF NOT EXISTS items(id SERIAL, market_hash_name text, icon_url text, market_price integer)");
+
+client.on('end', function() { client.end(); });
