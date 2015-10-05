c9666016dafd6e8cc60f42811452b11e97b6db82
Khoa Phan <khoaphan89@gmail.com>
2015-10-04 17:09:23 -0700

    Replace text with icons & popovers

--- client/public/js/angular/controller/NavCtrl.js
+++ client/public/js/angular/controller/NavCtrl.js
@@ -3,6 +3,10 @@
 angular.module('SnipeGo.NavCtrl', ['SnipeGo', 'SnipeGo.Services'])
   .controller('NavCtrl', ['$scope', '$rootScope', 'Auth', function($scope, $rootScope, Auth) {
 
+    $scope.playStop = 'play';
+
+    $scope.successDanger = 'success';
+
     $scope.isAuth = false;
 
     $scope.profilePic = '';
@@ -12,13 +16,23 @@ angular.module('SnipeGo.NavCtrl', ['SnipeGo', 'SnipeGo.Services'])
       Auth.checkSession().success(function(resp) {
         console.log('resp is ', resp);
         if (resp) {
-          $scope.profilePic = resp.photos[0].value;
+          $scope.profilePic = resp.photos[1].value;
           $scope.isAuth = true;
           $rootScope.user = resp;
         } else {
           console.log('there was no resp');
         }
       });
+    };
+
+    $scope.togglePlay = function() {
+      if ($scope.playStop === 'play') {
+        $scope.playStop = 'stop';
+        $scope.successDanger = 'danger';
+      } else {
+        $scope.playStop = 'play';
+        $scope.successDanger = 'success';
+      }
     };
 
     $scope.checkAuth();
