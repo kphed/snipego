a556cdbbf81d2d0b9dfaabee01d84ea3db7f0a68
Khoa Phan <khoaphan89@gmail.com>
2015-10-07 15:16:20 -0700

    Modify passport to work with firebase instead of postgres, edit navctrl to work with new passport object

--- passport/passport.js
+++ passport/passport.js
@@ -15,52 +15,28 @@ module.exports = function(passport) {
     done(null, id);
   });
 
-  var checkDatabase = function(done, profile) {
-    var ref = new Firebase('https://flickering-inferno-567.firebaseio.com/users');
-    // pg.connect(connectionString, function(err, client) {
-    //   if (err) {
-    //     console.log('error: ', err);
-    //   }
-    //   client.query('SELECT EXISTS(SELECT steamid FROM users WHERE steamid = ' + profile.id + ')', function(err, result) {
-    //     if (result.rows[0].exists) {
-    //       console.log('the user exists');
-    //       finish(done, profile);
-    //     } else {
-    //       client.query('INSERT INTO users(steamid, avatar) values ($1, $2)', [profile.id, profile.photos[1].value], function(err, result) {
-    //         console.log('user does not exist');
-    //         finish(done, profile);
-    //       });
-    //     }
-    //   });
-    // });
-  };
-
-  // var finish = function(done, profile) {
-  //   return done(null, profile);
-  // };
-
   passport.use(new SteamStrategy({
     returnURL: 'http://localhost:3000/users/auth/steam-callback',
     realm: 'http://localhost:3000/',
     apiKey: '49F43DF2D78801407F56520B9195E7F6'
   }, function(identifier, profile, done) {
-      var steamid = profile.id;
-      var photos = [profile.photos[0].value, profile.photos[1].value];
-
+      var steam = {
+        id: profile.id,
+        photos: [profile.photos[0].value, profile.photos[1].value],
+      };
       var userRef = new Firebase('https://flickering-inferno-567.firebaseio.com/users');
-      console.log('profile is ', profile);
       userRef.once('value', function(data) {
-        console.log('firebase data is ', data.val());
-        if (data.val() && data.val()[profile.id]) {
+        if (data.val() && data.val()[steam.id]) {
           console.log('this user exists in firebase!');
-          return done(null, data.val()[profile.id]);
+          return done(null, steam);
         } else {
           console.log('user does not exist, adding to database');
-          userRef.child(steamid).set({
-              photos: photos,
+          userRef.child(steam.id).set({
+              id: steam.id,
+              photos: steam.photos,
               tradeUrl: '',
           }, function() {
-            return done(null, profile);
+            return done(null, steam);
           });
         }
       });
