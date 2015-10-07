7257895780b0bff95712ec9435ab7cf6dab8479d
Khoa Phan <khoaphan89@gmail.com>
2015-10-07 11:47:56 -0700

    Add firebase npm module

--- client/public/js/angular/controller/MainCtrl.js
+++ client/public/js/angular/controller/MainCtrl.js
@@ -1,44 +1,38 @@
 'use strict';
 
 angular.module('SnipeGo.MainCtrl', ['SnipeGo'])
-  .controller('MainCtrl', ['$scope', '$window', function($scope, $window) {
+  .controller('MainCtrl', ['$scope', '$firebaseArray', '$rootScope', '$window', function($scope, $firebaseArray, $rootScope, $window) {
 
-    $scope.radius = '200';
+    var ref = new Firebase('https://flickering-inferno-567.firebaseio.com/messages');
+
+    $scope.messages = $firebaseArray(ref);
+    $scope.visible = true;
+    $scope.expandOnNew = true;
 
     $scope.getStyle = function() {
       var transform = 'translateY(-50%) ' + 'translateX(-50%)';
       return {
-          'top': '33%',
-          'bottom': 'auto',
-          'left': '50%',
-          'transform': transform,
-          '-moz-transform': transform,
-          '-webkit-transform': transform,
-          'font-size': 75 + 'px'
+        'top': '33%',
+        'bottom': 'auto',
+        'left': '50%',
+        'transform': transform,
+        '-moz-transform': transform,
+        '-webkit-transform': transform,
+        'font-size': 75 + 'px'
       };
     };
 
-    // $scope.vm = this;
+    $scope.sendMessage = function(message) {
+      console.log($scope.messages);
+      if(message && message !== '' && $rootScope.user) {
+        $scope.messages.$add({
+          'username': $rootScope.user.photos[0].value,
+          'content': message
+        });
+      } else {
+        $window.alert('There was an error sending your message. Please sign in or try again later.');
+      }
+    };
 
-  $scope.messages = [
-    {
-      'username': 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/62/626d9a8ffbe54581cb92c7a9e0cd7b2dba730e14.jpg',
-      'content': 'Hi!'
-    },
-  ];
-
-  $scope.username = 'username1';
-
-  $scope.sendMessage = function(message, username) {
-    if(message && message !== '' && username) {
-      $scope.messages.push({
-        'username': 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/62/626d9a8ffbe54581cb92c7a9e0cd7b2dba730e14.jpg',
-        'content': message
-      });
-    }
-  };
-  $scope.visible = true;
-  $scope.expandOnNew = true;
-
-  }]
+    }]
 );
