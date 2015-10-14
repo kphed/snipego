5a06c2bfeab428b85e064312e38fd7eb343142c8
Khoa Phan <khoaphan89@gmail.com>
2015-10-14 12:29:38 -0700

    Fix bot port

--- bot.js
+++ bot.js
@@ -45,7 +45,7 @@ var botInfo = {
   password: 'snipego123$',
   id: 1,
   name: 'supremekp',
-  port: 3017,
+  port: process.env.PORT,
   sentry: function() {
     if(fs.existsSync(__dirname + '/sentry/ssfn/' + botInfo.username + '.ssfn')) {
       var sha = require('crypto').createHash('sha1');
