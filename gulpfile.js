2b485b8b6bb385bbbf155b4771ffccc0579a4cee
Khoa Phan <khoaphan89@gmail.com>
2015-10-19 15:41:42 -0700

    PurifyCSS, minifyCSS

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var purify = require('gulp-purifycss');
+var minifyCSS = require('gulp-minify-css')
 
 gulp.task('scripts', function() {
   return gulp.src(['client/public/js/*.js',
@@ -15,16 +17,15 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('client/public/js'));
 });
 
-gulp.task('dependencies', function() {
-  return gulp.src(['client/public/bower_components/angular/angular.js',
-    'client/bower_components/jquery/dist/jquery.min.js',
-    'client/public/bower_components/underscore/underscore-min.js',
-    'client/public/bower_components/angular-ui-router/release/angular-ui-router.min.js',
-    'client/public/bower_components/angular-svg-round-progressbar/build/roundProgress.min.js',
-    'client/public/bower_components/angular-bootstrap-simple-chat/src/scripts/index.js'])
-    .pipe(concat('dependencies.js'))
+gulp.task('css', function() {
+  return gulp.src(['client/public/bower_components/bootstrap/dist/css/bootstrap.min.css',
+    'client/public/css/style.css',
+    'client/public/bower_components/angular-bootstrap-simple-chat/src/css/style.css',
+    'client/public/bower_components/angular-bootstrap-simple-chat/src/css/themes.css'])
+    .pipe(minifyCSS())
+    .pipe(purify(['client/public/js/*.html', 'client/public/js/template/*.html']))
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('client/public/js'));
-});
+    .pipe(gulp.dest('client/public/css/mini-css'));
+})
 
-gulp.task('default', ['scripts', 'dependencies']);
+gulp.task('default', ['scripts', 'css']);
