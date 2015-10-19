de96cf79534113bbde874c81eac945a35bdd4b4c
Khoa Phan <khoaphan89@gmail.com>
2015-10-19 15:21:40 -0700

    Attempt to concat dependencies pt. 1

--- gulpfile.js
+++ gulpfile.js
@@ -15,4 +15,16 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('client/public/js'));
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('dependencies', function() {
+  return gulp.src(['client/bower_components/jquery/dist/jquery.min.js',
+    'client/public/bower_components/underscore/underscore-min.js',
+    'client/public/bower_components/angular/angular.min.js',
+    'client/public/bower_components/angular-ui-router/release/angular-ui-router.min.js',
+    'client/public/bower_components/angular-svg-round-progressbar/build/roundProgress.min.js',
+    'client/public/bower_components/angular-bootstrap-simple-chat/src/scripts/index.js'])
+    .pipe(concat('dependencies.js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('client/public/js'));
+});
+
+gulp.task('default', ['scripts', 'dependencies']);
