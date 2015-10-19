445c187b87a84512dca863d50607d4e6e8003ad6
Khoa Phan <khoaphan89@gmail.com>
2015-10-19 11:54:08 -0700

    Deploy site

--- routes/deposit.js
+++ routes/deposit.js
@@ -7,15 +7,19 @@ var request = require('request');
 var jackpotRef = new Firebase('https://snipego.firebaseio.com/currentJackpot');
 
 router.post('/', function(req, res) {
+  console.log(' the body is ', req.body);
   jackpotRef.once('value', function(data) {
     var jackpotData = data.val();
     var url = 'http://steamcommunity.com/profiles/' + req.body.id + '/inventory/json/730/2';
-    request({
+    console.log('url is ', url);
+    request.get({
     url: url,
     json: true
     }, function(err, response, body) {
-      if (!err && response.statusCode === 200) {
+      console.log('boddyyy ', body);
+      if (!err) {
         for (var key in req.body.items) {
+          console.log('req.body.items ', req.body.items[key], key, req.body.items, body.rgInventory[key]);
           if (!body.rgInventory[key]) {
             res.json({'error': 'User is missing an item from their inventory'});
             return;
