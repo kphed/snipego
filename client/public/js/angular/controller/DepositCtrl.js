5fed501f21172035fcf500cbd1e605eb74fe3ff3
Khoa Phan <khoaphan89@gmail.com>
2015-10-20 16:32:19 -0700

    Deploy site

--- client/public/js/angular/controller/DepositCtrl.js
+++ client/public/js/angular/controller/DepositCtrl.js
@@ -3,7 +3,7 @@
 angular.module('SnipeGo.DepositCtrl', ['SnipeGo', 'SnipeGo.Services'])
   .controller('DepositCtrl', ['$scope', '$http', '$rootScope', '$window', '$firebaseObject', function($scope, $http, $rootScope, $window, $firebaseObject) {
 
-    var userSpecific = 'https://snipego.firebaseio.com/users/' + $rootScope.user.id;
+    var userSpecific = 'https://snipego.firebaseio.com/users';
 
     var userRef = new Firebase(userSpecific);
 
