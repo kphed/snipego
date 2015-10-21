c8ff8ae7ad59c488bdb1603b4d528a2a909da4ce
Khoa Phan <khoaphan89@gmail.com>
2015-10-21 11:44:43 -0700

    Deploy site

--- client/public/js/angular/controller/MainCtrl.js
+++ client/public/js/angular/controller/MainCtrl.js
@@ -28,7 +28,7 @@ angular.module('SnipeGo.MainCtrl', ['SnipeGo'])
     };
 
     $scope.getTwitch = function() {
-      return $http.get('https://api.twitch.tv/kraken/streams?game=Counter-Strike%3A%20Global%20Offensive&limit=1').success(function(resp) {
+      $http.get('https://api.twitch.tv/kraken/streams?game=Counter-Strike%3A%20Global%20Offensive&limit=1').success(function(resp) {
       var channelName = resp.streams[0].channel.display_name;
       channelName = channelName.replace(/['"]+/g, '');
 
