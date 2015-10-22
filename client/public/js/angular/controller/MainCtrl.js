f098d12756e0518000a919de544a162ddf7b79fb
Khoa Phan <khoaphan89@gmail.com>
2015-10-21 23:53:30 -0700

    Deploy site

--- client/public/js/angular/controller/MainCtrl.js
+++ client/public/js/angular/controller/MainCtrl.js
@@ -86,6 +86,7 @@ angular.module('SnipeGo.MainCtrl', ['SnipeGo'])
           for (var j = 0; j < $scope.ended[i].players.length; j++) {
             $scope.ended[i].players[j].chance = (($scope.ended[i].players[j].itemsValue / $scope.ended[i].jackpotValue) * 100).toFixed(2);
           }
+          $scope.ended[i].players = $scope.ended[i].players.reverse();
         }
       });
     });
