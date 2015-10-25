5d11edc1ac6bd7f0688f6f3c1c30bdf91d386f63
Khoa Phan <khoaphan89@gmail.com>
2015-10-24 22:05:18 -0700

    Change jackpot max to 30

--- jackpot.js
+++ jackpot.js
@@ -108,7 +108,7 @@ var queueJackpot = function(queueData) {
         jackpotValue: jackpotData.jackpotValue,
         players: jackpotData.players,
       }, function() {
-        if (jackpotData.itemsCount < 10) {
+        if (jackpotData.itemsCount < 30) {
           pollTimeout = setTimeout(function() {
             pollFirebaseQueue();
           }, 10000);
