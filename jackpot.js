0e793697bfef55901fcd86ac328d1fac51491edf
Khoa Phan <khoaphan89@gmail.com>
2015-10-26 13:44:59 -0700

    Deploy site

--- jackpot.js
+++ jackpot.js
@@ -148,7 +148,7 @@ var queueJackpot = function(queueData) {
         jackpotValue: jackpotData.jackpotValue,
         players: jackpotData.players,
       }, function() {
-        if (jackpotData.itemsCount < 30) {
+        if (jackpotData.itemsCount < 50) {
           pollTimeout = setTimeout(function() {
             pollFirebaseQueue();
           }, 10000);
