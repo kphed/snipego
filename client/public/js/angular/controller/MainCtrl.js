c4f07612169a3e40592c1cc71fb39f0f13ef8744
Khoa Phan <khoaphan89@gmail.com>
2015-10-26 09:16:03 -0700

    Deploy

--- client/public/js/angular/controller/MainCtrl.js
+++ client/public/js/angular/controller/MainCtrl.js
@@ -52,7 +52,7 @@ angular.module('SnipeGo.MainCtrl', ['SnipeGo'])
     $scope.getStyle = function() {
       var transform = 'translateY(-50%) ' + 'translateX(-50%)';
       return {
-        'top': '33%',
+        'top': '35%',
         'bottom': 'auto',
         'left': '50%',
         'transform': transform,
