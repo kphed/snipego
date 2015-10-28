0d2adc05ec5aa8ca972fea539af63f9193a5d8a1
Khoa Phan <khoaphan89@gmail.com>
2015-10-28 14:06:22 -0700

    Add winnings to user

--- jackpot.js
+++ jackpot.js
@@ -227,6 +227,19 @@ var endRound = function() {
                       pollFirebaseQueue();
                     }, 10000);
                   } else {
+                    ref.child('users').child(winnerObj.winner.id).once('value', function(data) {
+                      var userData = data.val();
+                      if (userData.won) {
+                        userData.won += winnerObj.winner.itemsValue;
+                      } else {
+                        userData.won = winnerObj.winner.itemsValue;
+                      }
+                      ref.child('users').child(winnerObj.winner.id).update({
+                        won: userData.won
+                      }, function() {
+                        console.log('Added winnings to user data');
+                      });
+                    });
                     console.log('Making a withdraw request now to bot');
                     pollTimeout = setTimeout(function() {
                       pollFirebaseQueue();
