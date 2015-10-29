5804e0e9497aa3ce7a883b6db8c7c204a84cbe96
Khoa Phan <khoaphan89@gmail.com>
2015-10-28 20:36:19 -0700

    Deploy site

--- jackpot.js
+++ jackpot.js
@@ -229,19 +229,19 @@ var endRound = function() {
                       pollFirebaseQueue();
                     }, 10000);
                   } else {
-                    // usersRef.child(winnerObj.winner.id).once('value', function(data) {
-                    //   var userData = data.val();
-                    //   if (userData && userData.won) {
-                    //     userData.won += Math.floor(parseFloat(winnerObj.winner.jackpotValue, 2));
-                    //   } else {
-                    //     userData.won = Math.floor(parseFloat(winnerObj.winner.jackpotValue, 2));
-                    //   }
-                    //   usersRef.child(winnerObj.winner.id).update({
-                    //     won: userData.won
-                    //   }, function() {
-                    //     console.log('Added winnings to user data');
-                    //   });
-                    // });
+                    usersRef.child(winnerObj.winner.id).once('value', function(data) {
+                      var userData = data.val();
+                      if (data.child('won').exists()) {
+                        userData.won += Math.floor(parseFloat(winnerObj.winner.jackpotValue, 2));
+                      } else {
+                        userData.won = Math.floor(parseFloat(winnerObj.winner.jackpotValue, 2));
+                      }
+                      usersRef.child(winnerObj.winner.id).update({
+                        won: userData.won
+                      }, function() {
+                        console.log('Added winnings to user data');
+                      });
+                    });
                     console.log('Making a withdraw request now to bot');
                     pollTimeout = setTimeout(function() {
                       pollFirebaseQueue();
